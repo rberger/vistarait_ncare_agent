1e87f9629bfb7be5ee1cf07a803db0724b46828f
Robert J. Berger <rberger@ibd.com>
2013-02-26 22:59:44 -0800

    Enabled the service

--- recipes/default.rb
+++ recipes/default.rb
@@ -52,8 +52,7 @@ remote_file pkg_destination do
   notifies :install, "package[#{pkg_name}]", :immediately
 end
 
-# services "ncare-agent" do
-#   supports :status => true, :restart => true
-#   action[ :enable, :start ]
-# end
-
+services "ncare-agent" do
+  supports :status => true, :restart => true
+  action[ :enable, :start ]
+end
